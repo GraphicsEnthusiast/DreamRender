2fc95e7b24f0a3b756b6ad5f050a2a8228007c52
qaz123w <1719990766@qq.com>
2024-01-09 00:01:49 +0800

    delete useless

--- core/xmake.lua
+++ core/xmake.lua
@@ -5,10 +5,9 @@ add_requires("stb")
 add_requires("tinyobjloader")
 add_requires("embree 3.13.5")
 add_requires("nlohmann_json")
---add_requires("openvdb 11.0.0")
 
 target("core")
     set_kind("static")
     add_files("*.cpp")
     add_packages("glm", "glfw", "glad", "stb", "tinyobjloader", 
-    "embree", --[["openvdb",--]] "nlohmann_json")
+    "embree", "nlohmann_json")
