8809ce93bfa1b11d252d0bbcbec91e24d1377dca
qaz123w <1719990766@qq.com>
2024-01-08 20:59:07 +0800

    add json dep

--- core/xmake.lua
+++ core/xmake.lua
@@ -4,8 +4,11 @@ add_requires("glad")
 add_requires("stb")
 add_requires("tinyobjloader")
 add_requires("embree 3.13.5")
+add_requires("nlohmann_json")
+--add_requires("openvdb 11.0.0")
 
 target("core")
     set_kind("static")
     add_files("*.cpp")
-    add_packages("glm", "glfw", "glad", "stb", "tinyobjloader", "embree")
+    add_packages("glm", "glfw", "glad", "stb", "tinyobjloader", 
+    "embree", --[["openvdb",--]] "nlohmann_json")
