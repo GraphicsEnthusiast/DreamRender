19143c76a2331b740a880aa28d29db384939a076
qaz123w <1719990766@qq.com>
2025-09-06 16:47:16 +0800

    add to world func

--- shader/util/util.glsl
+++ shader/util/util.glsl
@@ -23,4 +23,16 @@ vec3 ToLocal(vec3 vec, vec3 right, vec3 up, vec3 forward) {
     return vec3(dot(vec, right), dot(vec, up), dot(vec, forward));
 }
 
+/**
+ * @brief Converts a vector from local space (defined by basis vectors) to world space
+ * @param dir Vector in local space to transform
+ * @param right Local space right vector (typically the x-axis basis)
+ * @param up Local space up vector (typically the y-axis basis)
+ * @param forward Local space forward vector (typically the z-axis basis)
+ * @return Transformed vector in world space
+ */
+vec3 ToWorld(vec3 dir, vec3 right, vec3 up, vec3 forward) {
+    return dir.x * right + dir.y * up + dir.z * forward;
+}
+
 #endif // _UTIL__GLSL__
