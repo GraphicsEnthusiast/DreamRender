11ae71ba0fb231c1d2b6c5a4e824019dcecfb351
qaz123w <1719990766@qq.com>
2025-09-13 10:16:52 +0800

    Update interface.cpp

--- common/interface.cpp
+++ common/interface.cpp
@@ -267,7 +267,7 @@ void Interface::Render() {
 			while (rendering_active_) {
 				// Wait for GPU if a fence exists (max one frame in flight)
 				if (fence) {
-					GLenum result = glClientWaitSync(fence, 0, GL_TIMEOUT_IGNORED);
+					GLenum result = glClientWaitSync(fence, 0, 0);
 					glDeleteSync(fence);
 					fence = nullptr;
 				}
